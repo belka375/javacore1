43bd0a5e43202b5a51bab746cb15b77eea755ef7
Natallia Safonova <btcmp2019@outlook.com>
2019-09-09 20:04:34 -0700

    Add 5 Hey

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -5,5 +5,10 @@ public class Methods {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hey")
+        System.out.println("Hey")
+        System.out.println("Hey")
+        System.out.println("Hey")
+        System.out.println("Hey")
+        System.out.println("Hey")
     }
 }
