1551d0291a6a8c4987d6fa1d7ac3ce1e1dd85d70
TANIK STUDENT <tanik.student@gmail.com>
2019-09-10 23:21:22 -0700

    Hello from Tanik :)

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,21 +1,33 @@
 public class Methods {
     public static void main(String[] args) {
 
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
+    int[] chisla=new int[]{2,3,5,6,7,8};
 
+        System.out.println(sum(chisla));
 
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
+//    int[] chisla2=new int[]{3,9,1,8,4,8,5};
 
     }
-}
+
+    public static int sum(int numbers[]){
+        int summa =0;
+        for (int i=0; i<numbers.length; i++){
+            summa=summa+ numbers[i];
+        }
+        return summa;
+    }
+
+//    public static int[] sort(int numbers2[]){
+//        int min=numbers2[0];
+//        for (int numbers:numbers2) {
+//            if (numbers<min){
+//                if
+//                min=
+//            }
+//
+//
+//        }
+//
+//    }
+
+   }
