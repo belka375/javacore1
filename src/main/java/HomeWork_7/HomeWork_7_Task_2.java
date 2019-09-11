3aae81a9cf3ed483fb0a3c328d12be0b03f8d63f
jur4elo <jur4elo@yahoo.com>
2019-09-11 15:42:19 -0700

    HomeWork_7 Done

--- src/main/java/HomeWork_7/HomeWork_7_Task_2.java
+++ src/main/java/HomeWork_7/HomeWork_7_Task_2.java
@@ -5,8 +5,32 @@ public class HomeWork_7_Task_2 {
 
         int[] sort = {3, 9, 1, 8, 4, 8, 5};
         sortArrayElementsAsc(sort);
+        System.out.println("---------");
+        sortArrayElementsDesc(sort);
     }
 
+    //Sort DESC order
+    private static void sortArrayElementsDesc(int[] sort) {
+        int temp;
+        for (int i = 0; i < sort.length; i++) {
+            for (int j = 0; j < sort.length - 1; j++) {
+                if (sort[j] < sort[j + 1]) {
+                    temp = sort[j];
+                    sort[j] = sort[j + 1];
+                    sort[j + 1] = temp;
+                }
+            }
+        }
+        for (int desc : sort) {
+            System.out.println(desc);
+            {
+            }
+        }
+    }
+
+
+
+// Sort ASC order
     private static void sortArrayElementsAsc(int[] sort) {
         int temporary;
         for (int i = 0; i < sort.length; i++) {
@@ -18,18 +42,11 @@ public class HomeWork_7_Task_2 {
                 }
             }
         }
-        for (int noSorted : sort) {
-            System.out.println(noSorted);
+        for (int asc : sort) {
+            System.out.println(asc);
         }
     }
 }
-
-
-
-
-
-
-
 
 
 //import java.util.Arrays;
