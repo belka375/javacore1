fb90e8c7057cc66b9f149e3c024be7a4b16cd7fd
Baurzhan <baurzhan.zh@gmail.com>
2019-09-10 23:08:15 -0400

    HW7

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -2,40 +2,6 @@ public class Methods {
     public static void main(String[] args) {
 
         System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
 
     }
 }
