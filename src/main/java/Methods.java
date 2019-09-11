0eeb350e92b8a25a42e754245c54eb74db69ae86
irinamen <imendeleeva@testpro.io>
2019-09-10 23:29:57 -0700

    hw7

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -3,18 +3,18 @@ public class Methods {
         int[] numbers = {2,3,5,6,7,8};
         int[] numbers1 = {3,9,1,8,4,8,5};
 
-     //   arSumma(numbers);
+        arSumma(numbers);
         sorting(numbers1);
     }
 
 
-    //public static int arSumma(int[] numbers){
-    //    int sum = 0;
-    //    for (int num1 : numbers){
-    //        sum = sum + num1;
-    //    }
-    //    return sum;
-    //}
+    public static int arSumma(int[] numbers){
+        int sum = 0;
+        for (int num1 : numbers){
+            sum = sum + num1;
+        }
+        return sum;
+    }
     private static void sorting(int[] numbers1) {
         for (int i = 0; i < numbers1.length; i++) {
             for (int k = 0; k < numbers1.length; k++) {
