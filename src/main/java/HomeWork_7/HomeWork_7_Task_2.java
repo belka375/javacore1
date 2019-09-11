a219e8bdf13dc8b2ceddd661f3029f84146521e1
jur4elo <jur4elo@yahoo.com>
2019-09-11 15:18:20 -0700

    HomeWork_7 Done

--- src/main/java/HomeWork_7/HomeWork_7_Task_2.java
+++ src/main/java/HomeWork_7/HomeWork_7_Task_2.java
@@ -1,7 +1,5 @@
 package HomeWork_7;
 
-import java.util.Arrays;
-
 public class HomeWork_7_Task_2 {
     public static void main(String[] args) {
 
@@ -10,9 +8,43 @@ public class HomeWork_7_Task_2 {
     }
 
     private static void sortArrayElementsAsc(int[] sort) {
-        Arrays.sort(sort);
-        for (int i = 0; i < sort.length; i++) {
-            System.out.println(sort[i]);
+        int temporary;
+        for (int i = 0; i <= sort.length - 1; i++) {
+            for (int j = 0; j <= sort.length - 2; j++) {
+                if (sort[j] > sort[j + 1]) {
+                    temporary = sort[j];
+                    sort[j] = sort[j + 1];
+                    sort[j + 1] = temporary;
+                }
+            }
+        }
+        for (int noSorted : sort) {
+            System.out.println(noSorted);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+//import java.util.Arrays;
+//
+//public class HomeWork_7_Task_2 {
+//    public static void main(String[] args) {
+//
+//        int[] sort = {3, 9, 1, 8, 4, 8, 5};
+//        sortArrayElementsAsc(sort);
+//    }
+//
+//    private static void sortArrayElementsAsc(int[] sort) {
+//        Arrays.sort(sort);
+//        for (int i = 0; i < sort.length; i++) {
+//            System.out.println(sort[i]);
+//        }
+//    }
+//}
