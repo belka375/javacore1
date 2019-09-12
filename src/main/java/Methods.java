0d281070de5b703bf2bdee1c026a54146107e2ee
Irina256 <irina.skitayeva@gmail.com>
2019-09-11 20:42:23 -0400

    H/W 6

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,21 +1,30 @@
 public class Methods {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
 
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
+        sumNumbrs(a, b);
+        System.out.println("---------------");
 
+        int sumNumber = sum(a, b);
+        System.out.println(sumNumber);
+        System.out.println("---------------");
 
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
-        System.out.println("Hi");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, GIT");
-
+        boolean numIsEven = isEven(a);
+        System.out.println(numIsEven);
+        
     }
+    public static void sumNumbrs(int a, int b){
+        System.out.println("a+b="+(a+b) );
+    }
+    public static int sum (int a, int b){
+        return a+b;
+    }
+  public static boolean isEven(int a){
+        if(a%2==0){
+            return true;
+        }
+        else return false;
+            }
+
 }
