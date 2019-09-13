f5e04d52800691e6fcfa014623c68704eb148ab9
kg <kgold94@gmail.com>
2019-09-13 15:28:36 -0700

    hw8 class coffeemaker

---
+++ src/main/java/Hw8CoffeeMaker.java
@@ -0,0 +1,28 @@
+
+/*
+    * Homework 8.1-  Class and Method amd Object
+    * 1. Создайте 3 класса в которых есть аттрибуты и методы
+    * В классе Work создайте функцию main() и создайте в ней
+    * по 2 объекта каждого класса - покажите как они работают
+*/
+
+
+public class Hw8CoffeeMaker {
+
+    public String brand;
+
+    public String model;
+
+    public int year;
+
+
+    public void allabout() {
+
+        System.out.println("My coffee machine is: " + brand + ", " + "Model: " + model + " , " + "Made in :" + year);
+
+    }
+
+
+
+
+}
