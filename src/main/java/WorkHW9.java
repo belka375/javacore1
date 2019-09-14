11b45671959aeef609b4c247ebec1dbce7cf6eeb
klavdia <kgold94@gmail.com>
2019-09-14 11:02:25 -0700

    hw9 cellphone

---
+++ src/main/java/WorkHW9.java
@@ -0,0 +1,26 @@
+public class WorkHW9 {
+
+    public static void main(String[] args){
+
+        CellPhone cellOld =  new  CellPhone();
+
+        cellOld.setBrand("Apple");
+        cellOld.setModel("iPhone");
+        cellOld.setScreenSize(8.25);
+
+
+        String oldBrand = cellOld.getBrand();
+        String oldModel = cellOld.getModel();
+        double oldScreen = cellOld.getScreenSize();
+
+        System.out.println( "My old cell has parameters: " + oldBrand + " ," + " " + oldModel + " ,"  + " " + oldScreen ) ;
+
+
+
+
+
+
+    }
+
+
+}
