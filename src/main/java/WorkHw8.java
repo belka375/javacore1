199658da47518b6b1865a2ececd941fc0209bfc7
kg <kgold94@gmail.com>
2019-09-13 18:50:19 -0700

    hw8 class GradeCourse

---
+++ src/main/java/WorkHw8.java
@@ -0,0 +1,39 @@
+public class WorkHw8 {
+    public static void main(String[] args) {
+
+        Hw8CoffeeMaker mycf =  new  Hw8CoffeeMaker();
+        mycf.brand ="Nespresso";
+        mycf.year =2018;
+        mycf.model = "Precision Brewer";
+
+        mycf.allabout();
+
+//**********************************
+        Hw8Movie  myfavm = new Hw8Movie();
+        myfavm.genre = "musical drama";
+        myfavm.nominate = true;
+        myfavm.title = "My Fair Lady";
+        myfavm.year = 1964;
+
+        myfavm.myAction();
+        myfavm.relDate();
+//*************************************
+        Hw8GradeCourse mygrade = new  Hw8GradeCourse();
+        mygrade.retaken =false;
+        mygrade.grade = 10;
+        mygrade.coursename = "Java for kids";
+        mygrade.displayGreeting();
+        mygrade.gradeInfo();
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
