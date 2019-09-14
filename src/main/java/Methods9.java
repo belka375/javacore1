00e484706f5e99536a561686cf59ef157b9a8687
Dilya <deanabestom@gmail.com>
2019-09-14 10:48:29 -0700

    Lesson9

---
+++ src/main/java/Methods9.java
@@ -0,0 +1,22 @@
+public class Methods9 {
+    public static void main(String[] args) {
+
+        System.out.println("*************");
+        CellPhone cf=new CellPhone();
+        cf.setBrand("Nokia");
+        String str ="I am string";
+
+//       Book triDruga = new Book();
+//      triDruga.setName("Tri Druga");
+//      triDruga.setAuthor("Pushkin");
+//      triDruga.setBuyingPrice(-12);
+//      triDruga.setYearOfIssue(1550);
+//
+//      String bookname=triDruga.getName();
+//      String  bookAuthor=triDruga.getAuthor();
+//      int bPrice=triDruga.getBuyingPrice();
+//      int yr=triDruga.getYearOfIssue();
+
+//        System.out.println(bookname+", "+bookname+", "+bPrice+", "+yr);
+    }
+}
