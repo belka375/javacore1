5c5283fb0fb303992e6b2a3b5182d8f2ea2b4490
kg <kgold94@gmail.com>
2019-09-13 18:51:19 -0700

    hw8 class Movie

---
+++ src/main/java/Hw8GradeCourse.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Hw8GradeCourse {
+
+    String coursename;
+    int grade;
+    Boolean retaken;
+
+    public void displayGreeting()
+
+    {
+        System.out.println("Welcome to the Test Room!");
+    }
+
+    public void gradeInfo(){
+
+       System.out.println(" You passed a test: " + coursename + " " + " with grade = " + " " + grade );
+    }
+
+}
---
+++ src/main/java/Hw8Movie.java
@@ -0,0 +1,18 @@
+public class Hw8Movie {
+
+   String title;
+   Boolean nominate;
+   int year;
+   String genre;
+
+  static void myAction (){
+      System.out.println( "This movie was recorded");
+   }
+
+    public void relDate (){
+
+      System.out.println("Movie " + title + " " + "was release in  " + year);
+
+    }
+
+}
