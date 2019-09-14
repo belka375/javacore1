e6e811d936a4005eb3b16b3586041586b043459e
Kappa2014 <cola26071990@gmail.com>
2019-09-14 16:55:09 -0700

    HiFi.1

---
+++ src/main/java/HomeWork_7_3.java
@@ -0,0 +1,16 @@
+public class HomeWork_7_3 {
+    public static void main(String[] args) {
+        int[]numb2=new int[]{3, 9, 1, 8, 4, 8, 5};
+        System.out.println(getMinimum(numb2));
+    }
+    private static int getMinimum(int[]numb2){
+        int minimum=numb2[0];
+        for(int i=0;i<numb2.length;i++){
+            if(numb2[i]<minimum){
+                minimum=numb2[i];
+            }
+        }
+        return minimum;
+    }
+}
+
