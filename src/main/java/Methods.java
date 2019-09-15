c75b4a924283814e565bb648d499ab9f217a4743
Tatiana <tatianakuleva@gmail.com>
2019-09-15 14:26:43 -0400

    lesson 8

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,34 +1,68 @@
 public class Methods {
     public static void main(String[] args) {
 
-        System.out.println("**********************");
-
-
-        String[] subjs= new String[]{"Math","History","Biology","Social studies"};
-
-        Student john = new Student("John","Dole","Finance",subjs);
-        Student jane = new Student("Jane","Dole","Finance",subjs);
-        Student boris =new Student();
-        boris.setName("Boris");
-        boris.setLastName("Johnson");
-        boris.setMajor("Politics");
-        boris.setSubjects(subjs);
-
-        john.printStudentInfo();
-        jane.printStudentInfo();
-        boris.printStudentInfo();
-        String borisMajor=boris.getMajor();
-
-        boris.superSetter("ne Boris"," ne Jonson","New",subjs);
-        boris.printStudentInfo();
-
-//        CellPhone iPhone = new CellPhone("Apple", "11", 6);
-//        CellPhone samsungGalaxy=new CellPhone();
-//        iPhone.setScreenSize(5.5);
+//        System.out.println("**********************");
 //
-//        Book book1= new Book("Anchar");
-//        Book book = new Book();
-//        Book book3 = new Book("Bukvar'","Rumyanzev",15,2010);
+//
+//        String[] subjs= new String[]{"Math","History","Biology","Social studies"};
+//
+//        Student john = new Student("John","Dole","Finance",subjs);
+//        Student jane = new Student("Jane","Dole","Finance",subjs);
+//        Student boris =new Student();
+//        boris.setName("Boris");
+//        boris.setLastName("Johnson");
+//        boris.setMajor("Politics");
+//        boris.setSubjects(subjs);
+//
+//        john.printStudentInfo();
+//        jane.printStudentInfo();
+//        boris.printStudentInfo();
+//        String borisMajor=boris.getMajor();
+//
+//        boris.superSetter("ne Boris"," ne Jonson","New",subjs);
+//        boris.printStudentInfo();
+//
+////        CellPhone iPhone = new CellPhone("Apple", "11", 6);
+////        CellPhone samsungGalaxy=new CellPhone();
+////        iPhone.setScreenSize(5.5);
+////
+////        Book book1= new Book("Anchar");
+////        Book book = new Book();
+////        Book book3 = new Book("Bukvar'","Rumyanzev",15,2010);
+//
+//    }
+//}
+        Car mashina = new Car();
+        int[] ma = new int[10];
+
+        mashina.beep();
+
+//Car ivanCar=new Car();
+//ivanCar.brand="BMW";
+//ivanCar.model="X5";
+//ivanCar.color="Pink";
+//ivanCar.gasTankVolume=18.5;
+//
+//        ivanCar.lightOn();
+//        ivanCar.printCarInfo();
+//
+//
+//    }}
+
+//
+
+      Person waiter=new Person();
+          waiter.name="John";
+          waiter.lastName="Dole";
+          waiter.age=30;
+          waiter.eyeColor="Blue";
+          waiter.weight=100;
+          waiter.hasChildren=true;
+
+
+          waiter.printFullName();
+          waiter.printPhysicalCondition();
+
+      }
 
     }
-}
