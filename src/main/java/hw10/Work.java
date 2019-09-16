9b1bd599fb069ef7edc231ca507f88b35ad8c048
Kappa2014 <cola26071990@gmail.com>
2019-09-15 22:36:59 -0700

    hello

--- src/main/java/hw10/Work.java
+++ src/main/java/hw10/Work.java
@@ -2,8 +2,9 @@ package hw10;
 
 public class Work {
     public static void main(String[] args) {
-         Plane plane=new Plane();
-         plane.setPlane("\nBoeng");
+
+        Plane plane=new Plane();
+        plane.setPlane("\nBoeng");
          plane.setColor("Orange");
          plane.setSize("\nVery big one plane");
          plane.setDoors(11);
@@ -11,17 +12,12 @@ public class Work {
          plane.setFuelTankVolume(342.8);
          plane.printPlaneInfo();
 
-
         String[]crossStreets=new String[]{"Millenium Ave,","SE Walnut,","West Lane."};
         Road street=new Road();
         street.setCrossStreets(crossStreets);
         street.setLength(58.8);
         street.setRoad("Lomonosova");
         street.setSpeedLimit(35);
-
         street.printRoad();
-
-
-
     }
 }
