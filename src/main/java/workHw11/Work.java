428239a2539c946e4c670c09374334e669eaa2be
klavdia <kgold94@gmail.com>
2019-09-18 18:43:54 -0700

    Merge remote-tracking branch 'remotes/origin/master' into kg94
    # Conflicts:
    #	src/main/java/Hw9CellPhone.java
    #	src/main/java/Methods.java

---
+++ src/main/java/workHw11/Work.java
@@ -0,0 +1,20 @@
+package workHw11;
+
+/*
+ * Homework 11.1-  top casinos in UK
+ *Make enum (or 2 enum)
+  * Make two classes - in the first class attribute to make the enum
+   * in second class Grade - attribute type enum []
+*/
+
+public class Work {
+
+    public static void main (String arg[]){
+
+        System.out.println(java.util.Arrays.asList(Enumvalues.personInfo.values()));
+        System.out.println(java.util.Arrays.asList(Enumvalues.testList1.values()));
+
+
+    }
+
+}
