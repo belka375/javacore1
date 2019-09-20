ded24d29946855ddbedf466723531638363c653f
AnnaM <Millerankatun@gmail.com>
2019-09-19 23:29:33 -0700

    HW12

--- src/main/java/lesson11/Work.java
+++ src/main/java/lesson11/Work.java
@@ -3,13 +3,13 @@ package lesson11;
 public class Work {
     public static void main (String[] args) {
 
-        Size[] s = new Size[];
+      //  Size[] s = new Size[];
 
-        Pizza Margarita = new Pizza ("Margarita", s);
+    //    Pizza Margarita = new Pizza ("Margarita", s);
 
 
 
-        Sweatshirt Shirt = new Sweatshirt ("Gucci", Size.SMALL, 444);
+    //    Sweatshirt Shirt = new Sweatshirt ("Gucci", Size.SMALL, 444);
 
 
 
--- src/main/java/lesson12/Monitor.java
+++ src/main/java/lesson12/Monitor.java
@@ -1,4 +1,39 @@
 package lesson12;
 
 public class Monitor {
+    private String brand;
+    private double resolution;
+    private int size;
+
+    public Monitor (String brand, double resolution, int size) {
+
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setResolution(double resolution) {
+        this.resolution = resolution;
+    }
+
+    public double getResolution() {
+        return resolution;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void printMonitor () {
+        System.out.println(brand + " " + resolution + " " +size);
+    }
 }
