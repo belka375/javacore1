6ed458a3ce6457da224df290f1c3251216a2fb1e
tatyanaKotova <t_kotova72@mail.ru>
2019-09-20 08:17:11 -0400

    Have a good good night...

--- src/main/java/homeWork12/HomeWork12.java
+++ src/main/java/homeWork12/HomeWork12.java
@@ -3,14 +3,16 @@ package homeWork12;
 public class HomeWork12 {
     public static void main(String[] args) {
 
+
+
         Monitor monitor = new Monitor("Samsung", "Black", 17, 150.50);
         Keyboard keyboard = new Keyboard("Assus", "Black", "Wireless", 32.42);
         Mouse mouse = new Mouse("Assus", "Black", "wireless", 28.25);
         SystemBlock sb = new SystemBlock("Norway", "Black", 140.25);
-        sb.printSystemBlock();
-        mouse.printMouse();
-        keyboard.printKeyboard();
-        monitor.printMonitor();
+        
+        Computer myComp = new Computer("Tatyana Kotova", keyboard,mouse, monitor, sb);
+        myComp.printComputer();
+
 
     }
 }
