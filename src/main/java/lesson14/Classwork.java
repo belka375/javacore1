9b0d0b1d0c2c99274615e45486adb6d8bbfe960c
Dilya <deanabestom@gmail.com>
2019-09-21 15:22:49 -0700

    Lesson14

---
+++ src/main/java/lesson14/Classwork.java
@@ -0,0 +1,14 @@
+package lesson14;
+
+import java.time.LocalDateTime;
+
+public class Classwork {
+    public static void main(String[] args) {
+        Subject[] dashasSubject=new Subject[]{Subject.BIOLOGY,Subject.CHEMISTRY};
+        Student dasha=new Student( "Dasha", "Jennings", LocalDateTime.of(2002,1,1,1,0,0), Subject.CHEMISTRY);
+    }
+    }
+
+    }
+
+
---
+++ src/main/java/lesson14/Student.java
@@ -0,0 +1,22 @@
+package lesson14;
+
+import java.time.LocalDateTime;
+
+public class Student extends Person{
+    private Subject[] subjects;
+    private Subject major;
+
+    public Student(String firstName, String lastName, LocalDateTime dob, Subject[] subjects){
+        super(firstName,lastName,dob);
+        this.subjects=subjects;
+        this.major=major;
+    }
+
+    public Subject[] getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Subject[] subjects) {
+        this.subjects = subjects;
+    }
+}
