ffdf59207caab29f3342f59a78fb1271bb5590ea
LanaCas <msv7773@gmail.com>
2019-09-22 01:20:05 -0500

    Homew12

--- src/main/java/lesson12/Homework.java
+++ src/main/java/lesson12/Homework.java
@@ -4,16 +4,17 @@ public class Homework {
     public static void main(String[] args) {
 
 
-        Keyboard k = new Keyboard(12, Colors.BLACK, "Plastic");
-        Mouse mouse = new Mouse(Colors.RED);
-        Monitor mon = new Monitor("Acer", 12.0, 8);
-        SystemBlock sb = new SystemBlock(25, "Dell", 8);
+        Keyboard k = new Keyboard(10, Colors.BLACK, "Plastic");
+        Mouse mouse = new Mouse(Colors.WHITE);
+        Monitor mon = new Monitor("Samsung", 12.0, 12);
+        SystemBlock sb = new SystemBlock(32, "Philips", 12);
 
         Computer myDell = new Computer(k, mon, mouse, sb);
 
-        Keyboard kb = new Keyboard(15, Colors.BROWN, "Silicon");
-        Mouse ms = new Mouse(Colors.WHITE);
-        Monitor mnt = new Monitor("Dell", 14.0, 27);
+        Keyboard kb = new Keyboard(19, Colors.BLACK, "Metal");
+        Mouse ms = new Mouse(Colors.GREEN);
+        Monitor mnt;
+        mnt = new Monitor("Apple", 14.0, 27);
         SystemBlock sblck = new SystemBlock(24, "Dell", 10);
 
         Computer dell2 = new Computer(kb, mnt, ms, sblck);
