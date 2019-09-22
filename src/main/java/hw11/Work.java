7db844777b8d2dea7990811e1fe6e25a253f8608
brzjune <brzjune@gmail.com>
2019-09-22 07:32:01 -0500

    Merge remote-tracking branch 'remotes/origin/master' into yb16
    # Conflicts:
    #	src/main/java/hw8/Work.java

--- src/main/java/hw11/Work.java
+++ src/main/java/hw11/Work.java
@@ -5,13 +5,9 @@ public class Work {
 
     public static void main(String[] args) {
 
-       // Fruit apple = new Fruit("Apple", Fruit.Color.RED);
         Car tesla = new Car("Tesla", CarColors.PINK, new int[]{1, 3, 4});
         tesla.setColors(new CarColors[] {CarColors.GREEN, CarColors.ORANGE, CarColors.RED} );
-
-
-
-
+        
     }
 
 }
