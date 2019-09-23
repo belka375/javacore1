01b084cbf659c2db3bad84a88a0864efe99880a5
Enkidoothis <enkidoothis@gmail.com>
2019-09-23 02:37:40 -0400

    Done

--- src/main/java/hw13/Work.java
+++ src/main/java/hw13/Work.java
@@ -7,7 +7,7 @@ public class Work {
         Icebreaker ermak = new Icebreaker("Icbraker Disel", 36000, 54, "Liquid", "Ermak", 20241);
         LawnMover powerSmart = new LawnMover("Honda for lawn movers", 4, 1, "Air", "HRR216VKA", 2);
         Scooter hondaRuckus = new Scooter("Honda 49cc", 7, 1, "Liquid", "Ruckus", "Honda", 2, GoodTiers);
-        String test = hondaRuckus.getTiers().getTiresSize();
+        String test = hondaRuckus.getTiers().getTiresSize().getTireSize();
         System.out.println(test);
     }
 }
