83b307a5a16262f8577f925fd2e83bd523f7ff36
Tatyana_Kotova <t_kotova72@mail.ru>
2019-09-23 10:03:39 -0400

    hii

--- src/main/java/homeWork11/Work.java
+++ src/main/java/homeWork11/Work.java
@@ -3,48 +3,22 @@ package homeWork11;
 public class Work {
     public static void main(String[] args) {
 
-        Relative[] mySon=new Relative[]{new Relative(Sons.BIKA, 18), new Relative(Sons.AIBEK, 21), new Relative(Sons.IMAN,11)};
+       Relative aibek = new Relative(Sons.AIBEK, 21, "Swimming");
+       Relative bika = new Relative(Sons.BIKA, 18, "Boxing");
+       Relative iman = new Relative(Sons.IMAN, 11, "Dancing");
+               iman.setAge(12);
+               iman.setSport("Swimming");
+               aibek.setSport("Hiking");
 
-//        mySon[0] = new Relative(Sons.AIBEK, 21);
-//        mySon[1] = new Relative(Sons.BIKA, 18);
-//        mySon[2] = new Relative(Sons.IMAN, 11);
-        prinSon(mySon);
+        aibek.printSons();
+        System.out.println("=========================");
+        
+        bika.printSons();
+        System.out.println("=========================");
 
-
-        System.out.println("================================");
-
-        Cousins[] cousin = new Cousins[]{Cousins.NUKA, Cousins.NATALY, Cousins.NASTASYA, Cousins.MICHAEL, Cousins.LEO,
-                Cousins.BERIK, Cousins.ALIBEK};
-        printCousins(cousin);
-
-
-
-
-        RelativeAlmaty cous = new RelativeAlmaty(Cousins.ALIBEK, 10);
-        RelativeAlmaty cous2 = new RelativeAlmaty(Cousins.BERIK, 21);
-        RelativeAlmaty cous3 = new RelativeAlmaty(Cousins.LEO, 6);
-        RelativeAlmaty cous4 = new RelativeAlmaty(Cousins.MICHAEL, 11);
-        RelativeAlmaty cous5 = new RelativeAlmaty(Cousins.NASTASYA, 16);
-        RelativeAlmaty cous6 = new RelativeAlmaty(Cousins.NATALY, 30);
-        RelativeAlmaty cous7 = new RelativeAlmaty(Cousins.NUKA, 19);
-
+        iman.printSons();
+        System.out.println("=========================");
 
 
     }
-
-    private static void printCousins(Cousins[] cousin) {
-        for (Cousins x: cousin){
-            System.out.println(x);
-        }
-    }
-
-
-    private static void prinSon(Relative[] mySons){
-        for(Relative y: mySons){
-        System.out.println(y);
-    }
-
-
-
-}
 }
