8a6c6eaed681ea811e2ccabd5ad04f9b610d965f
Tatyana_Kotova <t_kotova72@mail.ru>
2019-09-23 09:47:19 -0400

    hii

--- src/main/java/homeWork11/RelativeAlmaty.java
+++ src/main/java/homeWork11/RelativeAlmaty.java
@@ -2,16 +2,45 @@ package homeWork11;
 
 public class RelativeAlmaty {
 
-    public static void main(String[] args) {
-        Cousins[] cousins = new Cousins[]{Cousins.ALIBEK, Cousins.BERIK, Cousins.LEO, Cousins.MICHAEL,
-                                             Cousins.NASTASYA,Cousins.NATALY, Cousins.NUKA};
-        printCousins(cousins);
+    private Cousins name;
+    private String lastName;
+    private int age;
+    private String mama;
+    private String papa;
+
+    public RelativeAlmaty(Cousins name, String lastName, int age, String mama, String papa){
+        this.lastName = lastName;
+        this.papa = papa;
+        this.mama = mama;
+        this.age = age;
+        this.name = name;
     }
 
-    private static void printCousins(Cousins[] cousins) {
-        for(Cousins x: cousins){
-            System.out.println(x);
-        }
+    public void setAge(int age) {
+        this.age = age;
     }
 
+    public String getPapa() {
+        return papa;
+    }
+
+    public String getMama() {
+        return mama;
+    }
+
+    public Cousins getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void printCousin(){
+        System.out.println("My Cousin is "+ name +" " + lastname+);
+    }
 }
