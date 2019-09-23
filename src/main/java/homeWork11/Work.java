b7f654f1eb1ab9383419a2ff5e71cf82a4c7feaf
Tatyana_Kotova <t_kotova72@mail.ru>
2019-09-23 10:28:51 -0400

    hii

--- src/main/java/homeWork11/Work.java
+++ src/main/java/homeWork11/Work.java
@@ -12,12 +12,48 @@ public class Work {
 
         aibek.printSons();
         System.out.println("=========================");
-        
+
         bika.printSons();
         System.out.println("=========================");
 
         iman.printSons();
         System.out.println("=========================");
+
+        RelativeAlmaty berik = new RelativeAlmaty(Cousins.BERIK, "Zh", 21, "Bota", "Marat");
+        RelativeAlmaty nuka = new RelativeAlmaty(Cousins.NUKA, "Zh", 18, "Bota", "Marat");
+        RelativeAlmaty alibek = new RelativeAlmaty(Cousins.ALIBEK, "Zh", 16, "Bota", "Marat");
+        RelativeAlmaty mika = new RelativeAlmaty(Cousins.MICHAEL, "Drachuk", 11, "Nadya", "Alex");
+        RelativeAlmaty leo = new RelativeAlmaty(Cousins.LEO, "Drachuk", 6, "Nadya", "Alex");
+        RelativeAlmaty nastya = new RelativeAlmaty(Cousins.NASTASYA, "Kotova", 16, "Katya", "Ivan");
+        RelativeAlmaty natali = new RelativeAlmaty(Cousins.NATALY, "Kotova", 28, "Olga", "Ivan");
+
+        berik.printCousin();
+        System.out.println("=========================");
+
+        nuka.printCousin();
+        System.out.println("=========================");
+
+        alibek.printCousin();
+        System.out.println("=========================");
+
+        mika.printCousin();
+        System.out.println("=========================");
+
+        leo.printCousin();
+        System.out.println("=========================");
+
+        nastya.printCousin();
+        System.out.println("=========================");
+
+        natali.printCousin();
+        System.out.println("=========================");
+ 
+
+
+
+
+
+
 
 
     }
