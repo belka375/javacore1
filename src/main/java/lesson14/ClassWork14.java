e8b3c160cc66db8e7c71daf74e4b6549330c92ff
Tatyana_Kotova <t_kotova72@mail.ru>
2019-09-23 20:19:56 -0400

    hii

---
+++ src/main/java/lesson14/ClassWork14.java
@@ -0,0 +1,18 @@
+package lesson14;
+
+import java.time.LocalDateTime;
+
+public class ClassWork14 {
+    public static void main(String[] args) {
+        Subject[] aibekSub = new Subject[]{Subject.CHEMISTRY, Subject.BIOLOGY};
+        Subject[] bikaSub = new Subject[]{Subject.PHYSICS, Subject.HISTORY};
+
+        Student aibek = new Student("Aibek", "Dosym", 1998, aibekSub, Subject.CHEMISTRY);
+        Student bika = new Student("Beybit", "Dosym", 2001, bikaSub, Subject.HISTORY);
+        Teacher baur = new Teacher("Baurzhan", "Dosym", 1975, Subject.MATH);
+        
+
+
+
+    }
+}
