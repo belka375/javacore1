e452990ced7de6e0e1ad83bee6bef429f8860165
irinamen <imendeleeva@testpro.io>
2019-09-24 20:15:01 -0700

    homework14

--- src/main/java/hw14/Work14.java
+++ src/main/java/hw14/Work14.java
@@ -9,15 +9,15 @@ public class Work14 {
         Seller nick= new Seller("Nick", "McMayers", 45776, "Liquid,Inc");
 
         mary.printCustomer();
-        System.out.println("-----------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
         nick.printSeller();
 
         //get
-        System.out.println("-----------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
         hw14.Currency[] currency=new hw14.Currency[]{hw14.Currency.UAH};
      Pack shop23=new Pack("Marina", "Nicson", 445322, "Ivan.LSD", 45, "Doll Kitty", Currency.UAH, DeliveryCity.KIEV, "airplane");
      shop23.printPack();
-        System.out.println("-----------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
      String name=shop23.getprodName();
      System.out.println(name);
 
