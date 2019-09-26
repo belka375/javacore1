b4a0e74f1452acb9e07aa25cf2db93759418d875
Kappa2014 <cola26071990@gmail.com>
2019-09-26 16:47:57 -0700

    hello

--- src/main/java/hw15/Work.java
+++ src/main/java/hw15/Work.java
@@ -6,11 +6,11 @@ public class Work {
         Suv honda=new Suv("Honda","Pilot",310,7);
         printCar(honda);
          Sedan nissan=new Sedan("Nissan","Altima",189,5);
-         printCar(nissan);
          Truck volvo= new Truck("Volvo",369,"White");
          printCar(volvo);
          Suv hyundai=new Suv("Hyundai","Santa Fe",298,7);
          printCar(hyundai);
+        nissan.printInfo();
     }
     private static void printCar(Auto car){
         car.printInfo();
