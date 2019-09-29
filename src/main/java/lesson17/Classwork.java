4c826b5d056daa7cbc716107df10ccb00a912d7e
Dilya <deanabestom@gmail.com>
2019-09-29 13:35:50 -0700

    Lesson16

--- src/main/java/lesson17/Classwork.java
+++ src/main/java/lesson17/Classwork.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Classwork {
+public class ClassWork {
     public static void main(String[] args) {
-        List<String> list=new ArrayList<String>();
+
+        List<String> list = new ArrayList<String>();
         list.add("Hello");
         list.add("it's");
         list.add("me");
@@ -15,38 +16,44 @@ public class Classwork {
 //        for (int i=0;i<list.size();i++){
 //            System.out.println(list.get(i));
 //        }
+//
 //        for (String str:list) {
 //            System.out.println(str);
 //        }
-//        list.forEach(str-> System.out.println(str));
+//
+//        list.forEach(str -> System.out.println(str));
+//
 //        list.forEach(System.out::println);
 
 //        if(list.contains("not")){
 //            System.out.println("we have NOT");
 //        }
-  //      list.remove(2);
-  //      list.forEach(System.out::println);
 
-   //     String[] arrayOfString=(String[])list.toArray();
+        list.remove(2);
+//        list.forEach(System.out::println);
 
-        HashMap <String,String> hm=new HashMap<String,String>();
-        hm.put("Apple", "green");
-        hm.put("Peach","red");
-        hm.put("Strawberry","red");
+//        String[] arrayOfStrings= (String[]) list.toArray();
+
+
+        HashMap<String,String> hm = new HashMap<String, String>();
+
+        hm.put("apple","green");
+        hm.put("peach","red");
+        hm.put("strawberry","red");
         hm.put("dill","green");
 
-        System.out.println(hm.get("Peach"));
+        System.out.println(hm.get("peach"));
 
-        HashMap<Integer,String> hm1=new HashMap<Integer,String>();
+        HashMap<Integer,String> hm1=new HashMap<Integer, String>();
         hm1.put(0,"Hello");
-        hm1.put(1,"it's");
+        hm1.put(1,"It's");
         hm1.put(2,"me");
 
         for(Integer i=0;i<hm1.size();i++){
             System.out.println(hm1.get(i));
         }
         hm1.put(55,"not");
-    //    hm1.remove(55);
+//        hm1.remove(55);
         hm1.remove(55,"not");
     }
 }
--- src/main/java/lesson18/Classwork.java
+++ src/main/java/lesson18/Classwork.java
@@ -1,24 +1,29 @@
 package lesson18;
 
+
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class Classwork {
+public class ClassWork {
     public static void main(String[] args) {
-        Map<String,String> countriesAndCapitals=new HashMap<>();
+        Map<String,String> countriesAndCapitals = new HashMap<>();
         countriesAndCapitals.put("UK","London");
-        countriesAndCapitals.put("Germany", "Berlin");
-        countriesAndCapitals.put("India", "New Delhi");
-        countriesAndCapitals.put("Australia", "Canberra");
+        countriesAndCapitals.put("Germany","Berlin");
+        countriesAndCapitals.put("India","New Delhi");
+        countriesAndCapitals.put("Australia","Canbera");
 
-        for (Map.Entry<String,String> pair:countriesAndCapitals.entrySet()) {
-            System.out.println("Country = "+pair.getKey()+", capital = "+pair.getValue());
+        for (Map.Entry<String,String> pair:countriesAndCapitals.entrySet()){
+            System.out.println("Country = "+ pair.getKey()+", capital = "+pair.getValue());
         }
-        countriesAndCapitals.forEach((key,value) ->{
-            System.out.println("Country = "+key+", capital = "+value);
+
+        countriesAndCapitals.forEach((key,value)->{
+            System.out.println("Country = "+ key+", capital = "+value);
         });
-       // Person Ivan=new Person();
+
+
+
+
+
     }
-
-
 }
