86d507a12301928b14224db2da8fc956d5eb47cd
klavdia <kgold94@gmail.com>
2019-10-16 07:22:21 -0700

    shw5

--- src/test/java/tests/TestRecord.java
+++ src/test/java/tests/TestRecord.java
@@ -1,45 +1,112 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static java.lang.Thread.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRecord {
     private WebDriver driver;
 
-    @AfterTest
-    public void tearDown() throws InterruptedException {
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException{
         Thread.sleep(7000);
         driver.quit();
     }
 
     @Test
-    public void tryToLogin_TryToOpenPage_TitlePageOpen()  {
-        System.setProperty("webdriver.chromedriver", "chromdriver");
-       driver = new ChromeDriver();
-        driver.get(("https://google.com"));
-        var title = driver.getTitle();
-        Assert.assertEquals(title, "Deens, plan my trip!", "Expected title is wrong");
-        }
+    public void searchInGoogLe_searchingForACar_CarFound() {
+        driver.get("http://google.com/");
 
-            //sleep(7000);
-           // driver.quit();
+            WebElement searchBox = driver.findElement(By.xpath("//*[@maxlength='2048']"));
+            searchBox.sendKeys("Car");
 
-    @Test public void tryToLogin_TryToOpenBing_TitlePageOpen()  {
-        System.setProperty("webdriver.chromedriver", "chromdriver");
-        driver = new ChromeDriver();
-        driver.get(("https://Bing.com"));
-        var title = driver.getTitle();
-        Assert.assertEquals(title, "Deens, plan my trip!", "Expected title is wrong");
+            Assert.assertEquals(searchBox.getAttribute("value"),"Car");
+
+            WebElement searchButton = driver.findElement(By.xpath("//*[@name='btnK']"));
+
+            Assert.assertTrue(searchButton.isDisplayed());
+
+
+
+//        try{
+//            WebElement searchBox = driver.findElement(By.xpath("//*[@maxlength='2048']"));
+//            searchBox.sendKeys("Car");
+//
+//            List<WebElement> searchButtons = driver.findElements(By.xpath("//*[@name='btnK']"));
+//            searchButtons.forEach(button->{
+//                if(button.isDisplayed()){
+//                    button.click();
+//                }
+//            });
+//        }catch(Exception x){
+//
+//        }
+
+
+//        searchBox.sendKeys(Keys.RETURN);
+
+//        WebElement searchButton = driver.findElement(By.xpath("(//*[@name='btnK'])[1]"));
+//        searchButton.click();
+
     }
 
+    @Test
+    public void deens_ClickLoginButton_LoginPageOpened() throws InterruptedException {
+        //Arrange
+        driver.get("https://deens-master.now.sh/");
 
+        //Act
+        WebElement loginButton = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginButton.click();
 
+        //Assert
+        Thread.sleep(2000);
+        WebElement loginHeader = driver.findElement(By.xpath("//*[@class='login-header']"));
+        String text=loginHeader.getText();
+        Assert.assertEquals(text,"Log-in to your account");
+    }
 
-   }
+    // //*[@class='login-img-content']
+    // //*[text()='Plan your next trip with us!']
+    // //*[contains(text(),'next trip')]
 
+    @Test
+    public void openUrl_TryToOpenDeens_GoogleOpened()  {
+        driver.get("https://google.com/");
+        var title = driver.getTitle();
+        Assert.assertEquals(title,"Deens, plan my trip!", "Expected title is wrong");
+    }
 
+    @Test
+    public void openUrl_TryToOpenDeens_BingOpened(){
+        driver.get("https://Bing.com/");
+        var title = driver.getTitle();
+        Assert.assertEquals(title,"Deens, plan my trip!", "Expected title is wrong");
+    }
+
+    @Test
+    public void openUrl_TryToOpenDeens_DeensOpened(){
+        driver.get("https://deens-master.now.sh/");
+        var title = driver.getTitle();
+        Assert.assertEquals(title,"Deens, plan my trip!", "Expected title is wrong");
+    }
+
+    @Test
+    public void openUrl_TryToOpenDeensCheckUrl_DeensOpened(){
+        driver.get("https://deens-master.now.sh/");
+        var url = driver.getCurrentUrl();
+        Assert.assertEquals(url,"https://deens-master.now.sh/", "Expected URL is wrong");
+    }
+
+}
