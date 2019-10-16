65efb9c0bae40c2b8cd18f88c5de4e9dd9d4a6ad
Irina256 <irina.skitayeva@gmail.com>
2019-10-16 16:05:54 -0400

    HW 6

---
+++ src/test/java/HW6.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HW6 {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void startUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedrivet.exe");
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(7000);
+        driver.quit();
+    }
+
+    @Test
+    public void deens_TryToLoginUsingWrongCredentials_LoginFailedErrorMessageAppear()throws InterruptedException {
+        driver.get("https://deens-master.now.sh/login");
+        Thread.sleep(7000);
+        WebElement email = driver.findElement(By.id("email"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        email.sendKeys("irina");
+        password.sendKeys("Irina256");
+
+        var buttonLogin = driver.findElement(By.xpath("//*[@data-testid='loginSubmit']"));
+
+        buttonLogin.click();
+        Assert.assertEquals("https://deens-master.now.sh/", "Error");
+
+
+    }
+}
