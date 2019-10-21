7b8ac8b3f96345a32740c4a9590f974e1f0fe1a9
Kappa2014 <cola26071990@gmail.com>
2019-10-21 14:24:43 -0700

    hello

---
+++ src/test/java/hw9/SignUpImplicitWait.java
@@ -0,0 +1,26 @@
+package hw9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignUpImplicitWait {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void deens() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
