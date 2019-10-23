993c021a2d13db84aff0e5bad66a6dc7254e5b8d
AnnaM <Millerankatun@gmail.com>
2019-10-22 18:48:22 -0700

    shw9

--- src/test/java/shw9/TestSignUp.java
+++ src/test/java/shw9/TestSignUp.java
@@ -14,12 +14,16 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
-@Test
+
 public class TestSignUp {
     private WebDriver driver;
     Wait<WebDriver> fluentWait;
+    Random rand = new Random();
+    int value = rand.nextInt(20);
 
+    @Test
     public void TryToSignUpUsingImplicitWait () throws InterruptedException{
         System.setProperty("webdriver.chrom.driver", "chromdriver");
         driver = new ChromeDriver();
@@ -30,13 +34,13 @@ public class TestSignUp {
         signUpButton.click();
 
         WebElement userField = driver.findElement(By.cssSelector("#username"));
-        userField.sendKeys("IvanIvanov980");
+        userField.sendKeys("IvanIvanov980" + value);
 
         WebElement emailField = driver.findElement(By.cssSelector("#email"));
-        emailField.sendKeys("ivanivanov88897000@mail.ru");
+        emailField.sendKeys("ivanivanov88897000@mail" + value +".ru");
 
         WebElement passwordField = driver.findElement(By.cssSelector("#password"));
-        passwordField.sendKeys("hnbDT78&&0980");
+        passwordField.sendKeys("hnbDT78&&0980"+ value);
 
         WebElement registerButton = driver.findElement(By.cssSelector(".green-btn"));
         registerButton.click();
@@ -50,41 +54,46 @@ public class TestSignUp {
     }
 
     @Test
-    public void TryToSignUpUsingFluentWait () throws InterruptedException {
-        System.setProperty("webdriver.chrom.driver", "chromdriver");
+  public void TryToSignUpUsingFluentWait () throws InterruptedException {
+       System.setProperty("webdriver.chrom.driver", "chromdriver");
         driver = new ChromeDriver();
-        fluentWait = new FluentWait<WebDriver>(driver)
-                .withTimeout(Duration.ofSeconds(20))
-                .pollingEvery(Duration.ofMillis(100))
-                .ignoring(NoSuchElementException.class);
+      fluentWait = new FluentWait<WebDriver>(driver)
+       .withTimeout(Duration.ofSeconds(20))
+           .pollingEvery(Duration.ofMillis(100))
+           .ignoring(NoSuchElementException.class);
 
-            driver.get("https://deens-master.now.sh/");
-            WebElement signUpButton = driver.findElement(By.xpath("//*[@href = '/register']"));
-            fluentWait.until(driver->driver.findElement(By.xpath("//*[@href = '/register']")).isEnabled());
-            signUpButton.click();
+           driver.get("https://deens-master.now.sh/");
+           fluentWait.until(driver -> driver.findElement(By.xpath("//*[@href = '/register']")).isDisplayed());
+           WebElement signUpButton = driver.findElement(By.xpath("//*[@href = '/register']"));
+          // fluentWait.until(driver->driver.findElement(By.xpath("//*[@href = '/register']")).isEnabled());
+           signUpButton.click();
 
             WebElement userField = driver.findElement(By.xpath("//*[@id ='username']"));
             fluentWait.until(driver->driver.findElement(By.xpath("//*[@id ='username']")));
-            userField.sendKeys("PetrPetrov9578");
+        Thread.sleep(3000);
+            userField.sendKeys("PetrPetrov9578" + value);
 
             WebElement emailField = driver.findElement(By.xpath("//*[@id ='email']"));
             fluentWait.until(driver-> driver.findElement(By.xpath("//*[@id ='email']")).isEnabled());
-            emailField.sendKeys("petr896615@inbox.ru");
+        Thread.sleep(3000);
+            emailField.sendKeys("petr896615@inbox" +value +".ru");
 
             WebElement passwordField = driver.findElement(By.xpath("//*[@id ='password']"));
-            fluentWait.until(driver-> driver.findElement(By.xpath("//*[@id ='email']")).isEnabled());
-            passwordField.sendKeys("8715uy0uiHG%$");
+           fluentWait.until(driver-> driver.findElement(By.xpath("//*[@id ='email']")).isEnabled());
+        Thread.sleep(3000);
+            passwordField.sendKeys("8715uy0uiHG%$" + value);
 
             WebElement registerButton = driver.findElement(By.xpath("//*[text() = 'Register']"));
             fluentWait.until(driver->driver.findElement(By.xpath("//*[text() = 'Register']")).isEnabled());
+        Thread.sleep(3000);
             registerButton.click();
 
             WebElement createTripButton = driver.findElement(By.xpath("//*[text() = 'Create Trip']"));
             Assert.assertTrue(createTripButton.isDisplayed());
 
-            driver.quit();
+          driver.quit();
 
 
 
-    }
+  }
 }
