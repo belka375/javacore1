dd35ed01b7793a91c0299c2cc5b0c696821d3272
Baurzhan <baurzhan.zh@gmail.com>
2019-10-25 23:33:04 -0400

    Selenium 12

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pageObjects.LandingPage;
+
+public class BaseTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException{
+        Thread.sleep(7000);
+        driver.quit();
+    }
+
+    protected LandingPage loginToApp(String username, String password) {
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.open();
+        var loginPage = landingPage.openLoginPage();
+        return loginPage.login("smarot10","Password_10");
+    }
+}
