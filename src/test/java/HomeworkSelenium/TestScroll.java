e13fae15435fd5e4a11a6b5a7f7955b9b96725bb
TANIK STUDENT <tanik.student@gmail.com>
2019-10-29 21:28:56 -0700

    1029

---
+++ src/test/java/HomeworkSelenium/TestScroll.java
@@ -0,0 +1,30 @@
+package HomeworkSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestScroll {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException{
+//        Thread.sleep(500);
+        driver.quit();
+    }
+
+    @Test
+    public void scroll_Click_CreateATrip (){
+        HomePage homePage = new HomePage(driver);
+        homePage.open();
+
+    }
+}
