88a8cc1c69e11d9d2de5700542d6dbb10f901abe
TANIK STUDENT <tanik.student@gmail.com>
2019-10-29 22:20:28 -0700

    SeleniumHomework 14

--- src/test/java/HomeworkSelenium/TestScroll.java
+++ src/test/java/HomeworkSelenium/TestScroll.java
@@ -1,13 +1,18 @@
 package HomeworkSelenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestScroll {
     WebDriver driver;
+    JavascriptExecutor js = (JavascriptExecutor)driver;
 
     @BeforeMethod
     public void startUp(){
@@ -22,9 +27,17 @@ public class TestScroll {
     }
 
     @Test
-    public void scroll_Click_CreateATrip (){
-        HomePage homePage = new HomePage(driver);
-        homePage.open();
+    public void scroll_Click_CreateATrip () throws InterruptedException {
+
+        driver.get("https://deens-master.now.sh");
+        Thread.sleep(500);
+        WebElement element = driver.findElement(By.xpath("//p[contains(text(),'Create a Trip')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+//      js.executeScript("window.scrollTo(0, document.body.scrollHeight)"); -> to scroll down the page
+        element.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://deens-master.now.sh/new/trip");
+        System.out.println("Expected URL: https://deens-master.now.sh/new/trip\nActual: "+URL);
 
     }
 }
