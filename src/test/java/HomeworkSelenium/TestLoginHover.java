0449b1e2c776babe5f763cd5e9ccc2d3b91faeca
TANIK STUDENT <tanik.student@gmail.com>
2019-11-04 20:16:26 -0800

    SeleniumHomework 14

---
+++ src/test/java/HomeworkSelenium/TestLoginHover.java
@@ -0,0 +1,10 @@
+package HomeworkSelenium;
+
+public class TestLoginHover {
+
+//    Actions action = new Actions(driver);
+//    WebElement we = driver.findElement(By.id("Add"));
+//    action.moveToElement(we).perform();
+//    assertEquals("#F67621", we.getCssValue("background-color"));
+
+}
