63572936751c8ceae9680deee07dd511748ef8c6
irinamen <imendeleeva@testpro.io>
2019-11-05 21:24:18 -0800

    seleniumhw14

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,25 +2,40 @@ package tests;
 
 import browserFactory.BrowserFactory;
 import enums.BrowserType;
-//import helpers.GetScreenshot;
+import helpers.GetScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import pageObjects.LandingPage;
+
+//import helpers.GetScreenshot;
 
 public class BaseTest {
     WebDriver driver;
+
+    @Parameters({"browser"})
     @BeforeMethod
-    public void startUp() throws NoSuchMethodException {
+
+    public void startUp(String browserName) throws NoSuchMethodException, NoSuchFieldError {
+        BrowserType browserType;
+        switch (browserName){
+            case "FIREFOX": browserType = BrowserType.FIREFOX;
+            break;
+            case "CHROME": browserType = BrowserType.CHROME;
+            break;
+            default: throw new NoSuchFieldError("NO such browser ");
+        }
+
         var factory = new BrowserFactory();
-        driver = factory.createWebDriver(BrowserType.CHROME);
+        driver = factory.createWebDriver(browserType);
         driver.manage().window().maximize();
     }
     @AfterMethod
     public void tearDown(ITestResult result) throws InterruptedException{
         if(result.getStatus()==ITestResult.FAILURE){
-//            GetScreenshot.capture(driver, result.getName());
+            GetScreenshot.capture(driver, result.getName());
         }
 
         Thread.sleep(7000);
